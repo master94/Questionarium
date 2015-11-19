7b96d649bb37b4ad2cbaf19e68b9db5ed3fd28fb
Valery Shevchuk <shevchuk.valery@gmail.com>
2015-11-20 00:18:12 +0200

    Add angularjs client app stub
    Also modified Rails configs to serve static data from angular
    app directory

---
+++ client/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name questionariumClientApp
+ * @description
+ * # questionariumClientApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('questionariumClientApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name questionariumClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the questionariumClientApp
+ */
+angular.module('questionariumClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
