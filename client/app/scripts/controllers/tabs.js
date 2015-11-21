fb143b99addfed0527c7a54706d3db2492e37e0c
Valery Shevchuk <shevchuk.valery@gmail.com>
2015-11-21 03:41:37 +0200

    Implement active tab highlighting
    - add tabs controller to store tabs list and active tab logic
    - modify main view to work with tabs controller

---
+++ client/app/scripts/controllers/tabs.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name questionariumClientApp.controller:TabsCtrl
+ * @description
+ * # TabsCtrl
+ * Controller of the questionariumClientApp
+ */
+angular.module('questionariumClientApp')
+  .controller('TabsCtrl', function ($scope, $location) {
+	  $scope.tabs = [
+	  	{
+			caption: 'Main',
+	  		route: '/'
+		},
+	  	{
+			caption: 'Questions',
+	  		route: '/questions'
+		},
+	  	{
+			caption: 'About',
+	  		route: '/about'
+		}
+	  ];
+
+	  $scope.tabs.forEach(function(tab) {
+		  tab.isActive = function() {
+			  return tab.route === $location.path();
+		  }
+	  });
+  });
