6ca49e601d9db0f2cad67e1b011efe1e0baf3f80
Valery Shevchuk <shevchuk.valery@gmail.com>
2015-12-12 15:24:56 +0200

    Add pagination for questions list
    - make visible only 10 questions per page

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'ng-token-auth'
+    'ng-token-auth',
+    'angularUtils.directives.dirPagination'
   ])
   .config(function ($routeProvider) {
     $routeProvider
