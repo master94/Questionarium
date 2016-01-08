159735f52610815ca58399a7cdeec9052e9a38cb
Valery Shevchuk <shevchuk.valery@gmail.com>
2016-01-08 20:11:48 +0200

    Sort questions by reversed creation order (newest first)

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,5 +1,5 @@
 class QuestionSerializer < ActiveModel::Serializer
-  attributes :id, :caption, :text 
+  attributes :id, :caption, :text, :created_at
   has_many :answers, serializer: AnswerSerializer
   has_one :user, serializer: UserSerializer
 end
